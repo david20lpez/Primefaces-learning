c7278e9f31d2b8e00fd5fa7568bda1e770ae4530
David López <david20lpez@utp.edu.co>
2019-02-28 12:23:19 -0500

    added charts example

--- target/generated-sources/annotations/com/utp/model/Nota_.java
+++ target/generated-sources/annotations/com/utp/model/Nota_.java
@@ -7,7 +7,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-02-25T17:30:36")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-02-28T12:18:57")
 @StaticMetamodel(Nota.class)
 public class Nota_ { 
 
