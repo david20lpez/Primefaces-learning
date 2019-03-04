9b3b10571b363db48f5cca9704fb275fabf42a36
David López <david20lpez@utp.edu.co>
2019-03-04 14:47:51 -0500

    fixed chart issues

--- src/main/java/com/utp/ejb/NotaFacade.java
+++ src/main/java/com/utp/ejb/NotaFacade.java
@@ -77,7 +77,7 @@ public class NotaFacade extends AbstractFacade<Nota> implements NotaFacadeLocal{
     public List<Object []> buscarPorCategoria()throws Exception{
         List<Object []> lista;
         try{
-            String jpql = "SELECT n.codigo, n.categoria.codigo, n.fecha FROM Nota n GROUP BY n.categoria.codigo, CAST(n.fecha AS DATE), n.codigo";
+            String jpql = "SELECT COUNT(n.codigo), n.categoria.codigo, n.fecha FROM Nota n GROUP BY n.categoria.codigo, CAST(n.fecha AS DATE)";
             Query query = em.createQuery(jpql);
            
             lista = query.getResultList();
